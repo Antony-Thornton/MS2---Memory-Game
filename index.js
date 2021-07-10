a941dc4f2754eed2de225972cf2d58796e0141c9
Antony-Thornton <Antony_thornton_p@outlook.com>
2021-07-10 15:34:22 +0000

    if_mapping checks

--- index.js
+++ index.js
@@ -155,65 +155,65 @@ function if_mapping_template(e) {
 
 
 function if_mapping(e) {
-  let selectedElement = e;
-  let selection_two = selectedElement.id;
-  let score = document.getElementById("correct_score");
-  let score_check = score.innerHTML;
-  let score_int = parseInt(score_check);
-  let max_score = 20;
-  let first_id = "front1";
-  let second_id = "front1";
+    let selectedElement = e;
+    let selection_two = selectedElement.id;
+    let score = document.getElementById("correct_score");
+    let score_check = score.innerHTML;
+    let score_int = parseInt(score_check);
+    let max_score = 20;
+    let first_id = "front1";
+    let second_id = "front1";
 
 
-  if (score_int === max_score) {
-      console.log("Game Complete")
-      /* 
-        Exit script - Game Complete 
-        Show Popup message
-        */
-  } else {
-      if (selection_one === "") {
-          selection_one = e.id;
-          console.log("Selection one = nothing selection_one updated")
-          /*
-          Add code to flip tile
-          update selection_one value
-          Exit Script
+    if (score_int === max_score) {
+        console.log("Game Complete")
+        /* 
+          Exit script - Game Complete 
+          Show Popup message
           */
-      } else {
-          if (selection_one === selection_two) {
-              console.log("matched tiles")
-              selection_one = "";
-              selection_two = "";
-              /* insert code to handle matched tiles
-              Update Incorrect clsss with + 1
-              Flip both tiles back to back            
-              */
-          }
-          if (score === 20) {
-              /* 
-              Exit script - Game Complete 
-              Show Popup message
-              */
-          } else {
+    } else {
+        if (selection_one === "") {
+            selection_one = e.id;
+            console.log("Selection one = nothing selection_one updated")
+            /*
+            Add code to flip tile
+            update selection_one value
+            Exit Script
+            */
+        } else {
+            if (selection_one === selection_two) {
+                console.log("matched tiles")
+                selection_one = "";
+                selection_two = "";
+                /* insert code to handle matched tiles
+                Update Incorrect clsss with + 1
+                Flip both tiles back to back            
+                */
+            }
+            if (score === 20) {
+                /* 
+                Exit script - Game Complete 
+                Show Popup message
+                */
+            } else {
 
-              if (selection_one !== selection_two && first_id === second_id) {
-                  console.log("Selection one and two DO NOT match")
+                if (selection_one !== selection_two && first_id === second_id) {
+                    console.log("Selection one and two DO NOT match")
 
-                  /* 
-                  If selection_one  does not equal selected Element insert code to handle tile flips 
-                  Update score class with +1
-                  Change both tiles to Locked              
-                  */
-                  selection_one = "";
-                  selection_two = "";
+                    /* 
+                    If selection_one  does not equal selected Element insert code to handle tile flips 
+                    Update score class with +1
+                    Change both tiles to Locked              
+                    */
+                    selection_one = "";
+                    selection_two = "";
 
 
 
-              }
-          }
-      }
-  }
+                }
+            }
+        }
+   }
 }
 
 
