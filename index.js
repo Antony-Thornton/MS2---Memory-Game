321356c32a30afb2a0d81c17cd4bd93c138443cd
Antony-Thornton <Antony_thornton_p@outlook.com>
2021-07-11 14:03:32 +0000

    if_mapping removed comments and fixed code format

--- index.js
+++ index.js
@@ -79,7 +79,6 @@ function reset_tiles() {
 
 
 
-
 function if_mapping(e) {
   let selectedElement = e;
   let selection_two = selectedElement.id;
@@ -87,101 +86,82 @@ function if_mapping(e) {
   let score_check = score.innerHTML;
   let score_int = parseInt(score_check);
   let max_score = 20;
-  let locked_check = e.className.substr(0,6)
+  let locked_check = e.className.substr(0, 6)
   console.log(locked_check)
 
-  if (locked_check = "locked"){
+  if (locked_check = "locked") {
 
-  if (score_int === max_score) {
-      console.log("Game Complete")
-      /* 
-        Exit script - Game Complete 
-        Show Popup message
-        */
-  } else {
-      if (selection_one === "") {
-          selection_one = e.id;
-          first_id = e.id.slice(-2);
-          console.log("Selection one = nothing so selection_one becomes e.id")
-          selectedElement.className = "back" + first_id;
-
-
-
-          /*
-          Add code to flip tile
-          update selection_one value
-          Exit Script
-          */
+      if (score_int === max_score) {
+          console.log("Game Complete")
+          /* 
+            Exit script - Game Complete 
+            Show Popup message
+            */
       } else {
-          if (selection_one === selection_two) {
-              console.log("Same tile selected")
-              /* below scoring adapted from the code instutute course */
-              old_score = parseInt(document.getElementById("incorrect_score").innerText);
-              document.getElementById("incorrect_score").innerText = ++old_score;
-              
-              selectedElement.className = "front";
+          if (selection_one === "") {
+              selection_one = e.id;
+              first_id = e.id.slice(-2);
+              console.log("Selection one = nothing so selection_one becomes e.id")
+              selectedElement.className = "back" + first_id;
 
-              selection_one = "";
-              selection_two = "";
-              first_id = "";
-              second_id = "";
-              /* insert code to handle matched tiles
-              Update Incorrect clsss with + 1
-              Flip both tiles back to back            
-              */
           } else {
-            second_id = e.id.slice(-2);
-              if (selection_one !== selection_two && first_id === second_id) {
-
-                  console.log("Selection one and two DO NOT match")
+              if (selection_one === selection_two) {
+                  console.log("Same tile selected")
                   /* below scoring adapted from the code instutute course */
-                  old_score = parseInt(document.getElementById("correct_score").innerText);
-                  document.getElementById("correct_score").innerText = ++old_score;
+                  old_score = parseInt(document.getElementById("incorrect_score").innerText);
+                  document.getElementById("incorrect_score").innerText = ++old_score;
                   /* End */
-                  
-                  selectedElement.className = "locked" + first_id;
-                  console.log(selection_one.className)
-                  let first_selection = document.getElementById(selection_one);
-                  first_selection.className = "locked" + first_id;
-                  /* 
-                  If selection_one  does not equal selected Element insert code to handle tile flips 
-                  Update score class with +1
-                  Change both tiles to Locked              
-                  */
+                  selectedElement.className = "front";
+
                   selection_one = "";
                   selection_two = "";
                   first_id = "";
                   second_id = "";
-
-                  score = document.getElementById("correct_score");
-                  score_check = score.innerHTML;
-                  score_int = parseInt(score_check);
-                  console.log(score_int)
-                  if (score_int === max_score) {
-                    console.log("Max score but mid function")
-                      /* 
-                      Exit script - Game Complete 
-                      Show Popup message
-                      */
-                  } 
-
               } else {
-                if (selection_one !== selection_two && first_id !== second_id)
-                selectedElement.className = "back" + second_id;
-                
-                setTimeout(function() {
-                  selectedElement.className = "front"
-                  reset_tiles()
-                }, 5000);
+                  second_id = e.id.slice(-2);
+                  if (selection_one !== selection_two && first_id === second_id) {
 
-                  /* add call reset tile function */
+                      console.log("Selection one and two DO NOT match")
+                      /* below scoring adapted from the code instutute course */
+                      old_score = parseInt(document.getElementById("correct_score").innerText);
+                      document.getElementById("correct_score").innerText = ++old_score;
+                      /* End */
+
+                      selectedElement.className = "locked" + first_id;
+                      console.log(selection_one.className)
+                      let first_selection = document.getElementById(selection_one);
+                      first_selection.className = "locked" + first_id;
+
+                      selection_one = "";
+                      selection_two = "";
+                      first_id = "";
+                      second_id = "";
+
+                      score = document.getElementById("correct_score");
+                      score_check = score.innerHTML;
+                      score_int = parseInt(score_check);
+                      console.log(score_int)
+                      if (score_int === max_score) {
+                          console.log("Max score but mid function")
+                          /* 
+                          Show Popup message
+                          */
+                      }
+
+                  } else {
+                      if (selection_one !== selection_two && first_id !== second_id)
+                          selectedElement.className = "back" + second_id;
+
+                      setTimeout(function() {
+                          selectedElement.className = "front"
+                          reset_tiles()
+                      }, 5000);
+                  }
               }
           }
       }
- }
+  }
 }
-}
-
 
 
 
